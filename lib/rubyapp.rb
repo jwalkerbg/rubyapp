89e98fd510a2e078224925836494eebf6f05d1ce
imc <i_cenov@botevgrad.com>
2024-11-08 11:11:24 +0200

    RT #100: Initial commit

---
+++ lib/rubyapp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubyapp/version"
+
+module Rubyapp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rubyapp/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rubyapp
+  VERSION = "0.1.0"
+end
