c5b27228ed1161f02a3597ef9dddbd1fb0fc2ce5
imc <i_cenov@botevgrad.com>
2024-11-08 23:39:13 +0200

    RT #100: Added main

--- lib/rubyapp.rb
+++ lib/rubyapp.rb
@@ -1,8 +1,41 @@
 # frozen_string_literal: true
 
+require "optparse"
+require "json"
+require_relative "rubyapp/config"
+require_relative "rubyapp/cli"
 require_relative "rubyapp/version"
 
 module Rubyapp
-  class Error < StandardError; end
-  # Your code goes here...
+  # Application Error handler
+  class Error < StandardError
+    attr_reader :message, :subject
+
+    def initialize(message, subject = '')
+      @message = message
+      @subject = subject
+      super()
+    end
+
+    # print_message composes and prints error message
+    def print_message
+      mess = @message
+      mess += ": #{@subject}" unless @subject.empty
+      puts mess
+    end
+  end
+
+  # Mainclass contains main function from which start app execution
+  class Mainclass
+    def main
+      # primary parse of the commandline options, handling -v --version option
+      # options = CommandLineParser.parse(ARGV)
+      # exit if CommandLineParser.optVersAndError(options)
+
+      cfg = Config.new()
+      puts cfg.default_config
+      args = CommandLineParser.parse
+      puts args
+    end
+  end
 end
