6b0a4f4d246a6b3400390b0d8594182399368cdf
imc <i_cenov@botevgrad.com>
2024-11-10 12:41:20 +0200

    RT #100: Add almost all code that handles options form defaults, config file and command line.

--- lib/rubyapp.rb
+++ lib/rubyapp.rb
@@ -2,6 +2,7 @@
 
 require "optparse"
 require "json"
+require "toml-rb"
 require_relative "rubyapp/config"
 require_relative "rubyapp/cli"
 require_relative "rubyapp/version"
@@ -28,14 +29,36 @@ module Rubyapp
   # Mainclass contains main function from which start app execution
   class Mainclass
     def main
-      # primary parse of the commandline options, handling -v --version option
-      # options = CommandLineParser.parse(ARGV)
-      # exit if CommandLineParser.optVersAndError(options)
-
-      cfg = Config.new()
-      puts cfg.default_config
+      cfg = Config.new
       args = CommandLineParser.parse
-      puts args
+      give_up if args.nil?
+      print_version if args["version"]
+
+      cfgfile = args["config"]&.then { |config| cfg.load_toml_file(config) }
+
+      # Merging options
+      cfgfile&.then { |cf| cfg.deep_merge(cf) }
+      cfg.merge_cli_options(args)
+
+      run_app(cfg.configuration)
+    end
+
+    def give_up
+      puts "Giving up."
+      exit
+    end
+
+    def print_version
+      puts "#{Rubyapp::NAME} #{Rubyapp::VERSION}"
+      exit
+    end
+
+    # Application code begins execution here, using configuration options as a hash object
+    def run_app(cfg)
+      puts "Running #{Rubyapp::NAME} with following options:"
+      puts cfg
+
+      # Implement application business logic here.
     end
   end
 end
--- lib/rubyapp/config.rb
+++ lib/rubyapp/config.rb
@@ -1,9 +1,15 @@
 # rubyapp/config
 
+require "toml-rb"
+require "json-schema"
+
 # Rubyapp - application nmodule
 module Rubyapp
   class Config
+    attr_accessor :configuration
+
     def initialize
+      @configuration = default_config
     end
 
     DEFAULT_CONFIG = {
@@ -21,8 +27,10 @@ module Rubyapp
       "logging" => {
         "verbose" => false
       },
-      "configuration" => {
-        # add options here, nested structur are available
+      "parameters" => {
+        # add options here, nested structures are available
+        "param1" => 1,
+        "param2" => 2
       }
     }
 
@@ -31,10 +39,9 @@ module Rubyapp
     end
 
     CONFIG_SCHEMA = {
-      "$schema" => "https://json-schema.org/draft/2020-12/schema",
       "type" => "object",
       "properties" => {
-        "metadata": {
+        "metadata" => {
           "type" => "object",
           "properties" => {
             "version" => {
@@ -54,7 +61,16 @@ module Rubyapp
         },
         "parameters" => {
           "type" => "object",
-          "additionalProperties" => true
+          "properties" => {
+            # Replace param1 and param2 with actual parameters in your application
+            "param1" => {
+              "type" => "integer"
+            },
+            "param2" => {
+              "type" => "number"
+            }
+          },
+          "additionalProperties" => false
         }
       },
       "additionalProperties" => false
@@ -64,15 +80,15 @@ module Rubyapp
       CONFIG_SCHEMA
     end
 
-    def self.load_toml_file(file_path, schema)
-      config = {}
+    def load_toml_file(file_path, schema=config_schema)
+      # config = {}
       begin
-        config = TomlRB.load_toml_file(file_path)
+        config = TomlRB.load_file(file_path)
       rescue TomlRB::ParseError => e
         puts "TOML parsing error: #{e.message}"
         return nil
       rescue Errno::ENOENT => e
-        puts "File not found: #{file_path}"
+        puts "File not found: #{file_path}, #{e.message}"
         return nil
       rescue StandardError => e
         puts "An unexpected error occurred: #{e.message}"
@@ -83,11 +99,52 @@ module Rubyapp
       begin
         JSON::Validator.validate!(schema, config)
         puts "Configuration is valid."
-        config  # Return the loaded configuration
+        config # Return the loaded configuration
       rescue JSON::Schema::ValidationError => e
         puts "Validation error: #{e.message}"
         nil
       end
     end
+
+    def deep_merge(override)
+      self.configuration = _deep_merge(configuration, override)
+    end
+
+    def _deep_merge(original, override)
+      original.merge(override) do |_, old, new|
+        old.is_a?(Hash) && new.is_a?(Hash) ? _deep_merge(old, new) : new
+      end
+    end
+
+    def merge_cli_options(args)
+      return unless args
+
+      # List of possible parameters
+      # This works if there is only one level of nesting (here category).
+      params = {
+        "verbose" => "logging",
+        "version" => "metadata",
+        # replace param1 and param2 with actual parameters in your application
+        "param1" => "parameters",
+        "param2" => "parameters"
+      }
+
+      params.each do |key, category|
+        configuration[category][key] = args[key] if args.include?(key)
+      end
+    end
+
+=begin
+    variant that allow individual copy with multiple levels of nesting in configuration
+    def merge_cli_options(args)
+      return unless args
+
+      configuration["logging"]["verbose"] = args["verbose"] if args.include?("verbose")
+      configuration["metadata"]["version"] = args["version"] if args.include?("version")
+      # replace param1 and param2 with actual parameters in your application
+      configuration["parameters"]["param1"] = args["param1"] if args.include?("param1")
+      configuration["parameters"]["param2"] = args["param2"] if args.include?("param2")
+    end
+=end
   end
 end
