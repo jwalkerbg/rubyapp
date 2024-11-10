ea5ad01eab373ddc5aad66fd2a39cc7c01d68fb4
imc <i_cenov@botevgrad.com>
2024-11-10 18:13:45 +0200

    RT #100: Formatted cli.app

--- lib/rubyapp/cli.rb
+++ lib/rubyapp/cli.rb
@@ -10,26 +10,34 @@ module Rubyapp
 
       opt_parser = OptionParser.new do |opt|
         opt.banner = "Usage: rubyapp [options...\n\n]"
+
         opt.on("--config FILE", String, "Specify the configuration file") do |file|
           parameters["config"] = file
         end
+
         opt.on("--no-config", "Disable the use of a configuration file") do
           parameters["config"] = nil
         end
+
         opt.on_tail("-V", "--version", "app version") do
           parameters["version"] = true
         end
+
         opt.on_tail("--[no-]verbose", "Verbose logging (default: --no-verbose)") do |v|
           parameters["verbose"] = v
         end
+
         # Replace param1 and param2 with actual options for your application
+
         opt.on("--param1 VALUE", Integer, "Parameter1") do |v|
           parameters["param1"] = v
         end
+
         opt.on("--param2 VALUE", Integer, "Parameter2") do |v|
           parameters["param2"] = v
         end
       end
+
       begin
         opt_parser.parse!
       rescue OptionParser::InvalidOption, OptionParser::NeedlessArgument, OptionParser::InvalidArgument,
@@ -37,6 +45,7 @@ module Rubyapp
         logger.error(e)
         parameters = nil
       end
+
       parameters
     end
   end
